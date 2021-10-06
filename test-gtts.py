e72b5bef1e9e647efbf117fe28a0f841fb8eede7
Nikos Zounis <armados@gmail.com>
2021-10-06 08:55:52 +0300

    add mp3 folder

--- test-gtts.py
+++ test-gtts.py
@@ -4,16 +4,16 @@ from gtts import gTTS
 
 tts = gTTS('Καλημέρα Κόσμε!', lang='el')
 
-tts.save('1epalaudio.mp3')
+tts.save('mp3s/1epalaudio.mp3')
 
 
 tts = gTTS('Hello world!', lang='en')
 
-tts.save('1epalaudio_en.mp3')
+tts.save('mp3s/1epalaudio_en.mp3')
 
 
 
 tts = gTTS('welcome to 1epal pyrgou!', lang='en')
 
-tts.save('1epalaudio_en2.mp3')
+tts.save('mp3s/1epalaudio_en2.mp3')
 
