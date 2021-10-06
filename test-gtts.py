66c4a33925ea7d2c5ac41ec51d362ad14dc8436e
Nerozar03 <chrisbogos03@gmail.com>
2021-10-06 08:52:16 +0300

    add second messege

--- test-gtts.py
+++ test-gtts.py
@@ -10,3 +10,10 @@ tts.save('1epalaudio.mp3')
 tts = gTTS('Hello world!', lang='en')
 
 tts.save('1epalaudio_en.mp3')
+
+
+
+tts = gTTS('welcome to 1epal pyrgou!', lang='en')
+
+tts.save('1epalaudio_en2.mp3')
+
